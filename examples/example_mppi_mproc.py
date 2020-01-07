b2b92752e69d7953dafd9848abde4eb5c19918a8
Mohak Bhardwaj <mohak.bhardwaj@gmail.com>
2020-01-06 16:47:58 -0800

    added proper logging to examples

--- examples/example_mppi_mproc.py
+++ examples/example_mppi_mproc.py
@@ -2,6 +2,7 @@
 import sys, os
 sys.path.insert(0, os.path.abspath('..'))
 import argparse
+from datetime import datetime
 import gym
 import numpy as np
 import tqdm
@@ -31,11 +32,17 @@ parser.add_argument('--gamma', type=float, default=1.0, help='discount factor')
 parser.add_argument('--n_iters', type=int, default=1, help='number of update steps per iteraion of mpc')
 parser.add_argument('--seed', type=int, default=0, help='number of samples per planning iteration')
 parser.add_argument('--render', action='store_true', help='render environment')
+parser.add_argument('--save_dir', type=str, default='/tmp', help='folder to save data in')
+
 args = parser.parse_args()
 
-#Setiup logging
-LOG_DIR = os.path.abspath("..")
-logger.setup("MPPI", os.path.join(LOG_DIR, 'log.txt'), 'debug')
+#Setup logging
+now = datetime.now()
+date_time = now.strftime("%m_%d_%Y_%H_%M_%S")
+exp_name = 'MPPI_{0}'.format(args.env)
+LOG_DIR = os.path.join(os.path.abspath(args.save_dir), exp_name + "/" + date_time)
+if not os.path.exists(LOG_DIR): os.makedirs(LOG_DIR)
+logger.setup(exp_name, os.path.join(LOG_DIR, 'log.txt'), 'debug')
 
 #Create the main environment
 np.random.seed(args.seed)
@@ -134,6 +141,7 @@ timeit.stop('start')
 logger.info(timeit)
 logger.info('Buffer Length = {0}'.format(len(buff)))
 logger.info('Average reward = {0}. Closing...'.format(np.average(ep_rewards)))
+buff.save(LOG_DIR)
 
 if args.render:
     logger.info('Rendering 2 times')
