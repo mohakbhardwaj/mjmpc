6e19212159f1790c45cb65d5b6a336b4bc7e94f0
Mohak Bhardwaj <mohak.bhardwaj@gmail.com>
2020-02-05 23:45:22 -0800

    added setup.py, need to test and populate readme

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info.major != 3:
+    print("This Python is only compatible with Python 3, but you are running "
+          "Python {}. The installation will likely fail.".format(sys.version_info.major))
+    
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='mjmpc',
+    version='1.0.0',
+    packages=find_packages(),
+    description='Model-Predictive Control (MPC) algorithms for environments in MuJoCo',
+    long_description=read('README.md'),
+    url='https://github.com/mohakbhardwaj/mjmpc.git',
+    author='Mohak Bhardwaj',
+    install_requires=[
+        'click', 
+        'gym>=0.13', 
+        'mujoco_py>=2.0', 
+        'tqdm', 
+        'numpy',
+        'colorlog',
+        'tabulate',
+        'pandas',
+    ],
+)
