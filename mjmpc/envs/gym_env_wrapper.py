8c536626e7c3b143f0c84aedb50d35b3b271c77e
Nolan Wagener <nolan.wagener@gmail.com>
2021-09-10 10:47:30 -0400

    Formatting

--- mjmpc/envs/gym_env_wrapper.py
+++ mjmpc/envs/gym_env_wrapper.py
@@ -25,7 +25,8 @@ class GymEnvWrapper():
             self.d_obs = 0.
             for k in observation.keys():
                 self.d_obs += observation[k].size
-        else: self.d_obs = observation.size
+        else:
+            self.d_obs = observation.size
         state = self.get_env_state()
         if type(state) is tuple:
             self.d_state = np.sum([o.size for o in state])
@@ -45,14 +46,14 @@ class GymEnvWrapper():
 
         self.default_dyn_params = defaultdict(dict)
         self.randomized_dyn_params = defaultdict(dict)
-        
+
         self.seed()
         super(GymEnvWrapper, self).__init__()
 
     def step(self, u):
         obs, rew, done, info = self.env.step(u)
         return deepcopy(obs), deepcopy(rew), deepcopy(done), deepcopy(info)
- 
+
     def set_env_state(self, state_dict: dict):
         """
         Set the state of environment from dictionary
@@ -61,7 +62,7 @@ class GymEnvWrapper():
             self.env.set_env_state(state_dict)
         except:
             self.env.env.set_env_state(state_dict)
- 
+
     def get_env_state(self) -> dict:
         """
         Return dictionary of the full environment state
@@ -79,7 +80,7 @@ class GymEnvWrapper():
             return self.env.get_obs().copy()
         except:
             return self.env.env.get_obs().copy()
- 
+
     def get_reward(self, state, action, next_state):
         '''
         Return the reward function for the transition
@@ -88,14 +89,14 @@ class GymEnvWrapper():
 
     def rollout(self, batch_size: int, horizon: int, mean: np.ndarray, noise: np.ndarray = None, mode="open_loop"):
         """
-        Given batch of action sequences, we perform rollouts 
+        Given batch of action sequences, we perform rollouts
         and return resulting observations, rewards etc.
 
         Parameters
         ----------
         mean: if mode == "open_loop"
                 np.ndarray of shape [horizon, d_act] if open_loop
-              elif mode == "closed_loop" 
+              elif mode == "closed_loop"
                 np.ndarray of shape [horizon, d_obs, d_act]
         :return:
             obs_vec: np.ndarray [batch_size, horizon, d_obs]
@@ -104,16 +105,16 @@ class GymEnvWrapper():
             done_vec: np.ndarray [batch_size, horizon, 1]
             info: dict
 
-        TODO: Add conditions: 
+        TODO: Add conditions:
                 if noise is None to run only mean
-                if noise is weight_matrix, we sample using re-param trick 
+                if noise is weight_matrix, we sample using re-param trick
         """
         start_t = time.time()
         # if mode == "open_loop":
             # batch_size, horizon, d_action = noise.shape
         if type(self.observation_space) is spaces.Dict:
             obs_vec = []; next_obs_vec = []
-        else: 
+        else:
             obs_vec = np.zeros((batch_size, horizon, self.d_obs))
             next_obs_vec = np.zeros((batch_size, horizon, self.d_obs))
         state_vec = [] #np.zeros((self.batch_size, horizon, self.d_state))
@@ -136,7 +137,7 @@ class GymEnvWrapper():
                     mean_act = mean.T @ np.append(curr_obs,1.0)
                 if noise is not None:
                     u_curr = mean_act + noise[b, t, :]
-                else: 
+                else:
                     u_curr = mean_act
                 next_obs, rew, done, _ = self.step(u_curr)
                 if type(self.observation_space) is spaces.Dict:
@@ -148,7 +149,7 @@ class GymEnvWrapper():
                 # state = self.get_env_state()
                 # state_vec.append(state.copy())
                 rew_vec[b, t] = rew
-                act_vec[b,t, :] = u_curr.copy().reshape(self.d_action,) 
+                act_vec[b,t, :] = u_curr.copy().reshape(self.d_action,)
                 done_vec[b, t] = done
                 curr_obs = next_obs.copy()
 
@@ -157,7 +158,7 @@ class GymEnvWrapper():
 
     # def rollout(self, mean: np.ndarray):
     #     """
-    #     Given batch of action sequences, we perform rollouts 
+    #     Given batch of action sequences, we perform rollouts
     #     and return resulting observations, rewards etc.
     #     :param u_vec: np.ndarray of shape [batch_size, horizon, d_action]
     #     :return:
@@ -171,7 +172,7 @@ class GymEnvWrapper():
     #     batch_size, horizon, d_action = u_vec.shape
     #     if type(self.observation_space) is spaces.Dict:
     #         obs_vec = []; next_obs_vec = []
-    #     else: 
+    #     else:
     #         obs_vec = np.zeros((batch_size, horizon, self.d_obs))
     #         next_obs_vec = np.zeros((batch_size, horizon, self.d_obs))
     #     state_vec = [] #np.zeros((self.batch_size, horizon, self.d_state))
@@ -203,10 +204,10 @@ class GymEnvWrapper():
     #     info = {'total_time' : time.time() - start_t}
     #     return obs_vec, rew_vec, done_vec, info, next_obs_vec
 
-    
+
     # def rollout(self, u_vec: np.ndarray):
     #     """
-    #     Given batch of action sequences, we perform rollouts 
+    #     Given batch of action sequences, we perform rollouts
     #     and return resulting observations, rewards etc.
     #     :param u_vec: np.ndarray of shape [batch_size, horizon, d_action]
     #     :return:
@@ -220,7 +221,7 @@ class GymEnvWrapper():
     #     batch_size, horizon, d_action = u_vec.shape
     #     if type(self.observation_space) is spaces.Dict:
     #         obs_vec = []; next_obs_vec = []
-    #     else: 
+    #     else:
     #         obs_vec = np.zeros((batch_size, horizon, self.d_obs))
     #         next_obs_vec = np.zeros((batch_size, horizon, self.d_obs))
     #     state_vec = [] #np.zeros((self.batch_size, horizon, self.d_state))
@@ -251,14 +252,14 @@ class GymEnvWrapper():
 
     #     info = {'total_time' : time.time() - start_t}
     #     return obs_vec, rew_vec, done_vec, info, next_obs_vec
-    
+
     def rollout_cl(self, policy, batch_size, horizon, mode='mean', noise=None):
         """
-        Given a policy object, we perform rollouts 
+        Given a policy object, we perform rollouts
         and return resulting trajectroy.
         Parameters
         ----------
-        policy: 
+        policy:
         batch_size: number of rollouts
         horizon: rollout horizon
 
@@ -276,7 +277,7 @@ class GymEnvWrapper():
             # raise ValueError('Added noise must be None when using sample mode from policy')
         if type(self.observation_space) is spaces.Dict:
             obs_vec = []; next_obs_vec = []
-        else: 
+        else:
             obs_vec = np.zeros((batch_size, horizon, self.d_obs))
             next_obs_vec = np.zeros((batch_size, horizon, self.d_obs))
         act_vec = np.zeros((batch_size, horizon, self.d_action))
@@ -297,7 +298,7 @@ class GymEnvWrapper():
                     obs_torch = torch.FloatTensor(curr_obs).unsqueeze(0)
                     noise_sample = noise[b,t] if noise is not None else None
                     u_curr, act_info = policy.get_action(obs_torch, mode, noise_sample)
-                    u_curr = u_curr.numpy().copy().reshape(self.d_action,) 
+                    u_curr = u_curr.numpy().copy().reshape(self.d_action,)
                     # log_prob = log_prob.numpy().item()
                     inf_time = time.time() - before_inf
                     total_inf_time += inf_time
@@ -306,7 +307,7 @@ class GymEnvWrapper():
                         # u_curr = u_curr + noise[b,t]
                     #step environment
                     next_obs, rew, done, _ = self.step(u_curr)
-                    
+
                     #collect data
                     if type(self.observation_space) is spaces.Dict:
                         obs_vec.append(curr_obs.copy())
@@ -323,10 +324,10 @@ class GymEnvWrapper():
 
         info = {'total_time' : time.time() - start_t, 'inference_time': total_inf_time}
         return obs_vec, act_vec, act_infos, rew_vec, done_vec, next_obs_vec, info
-    
+
     def seed(self, seed=None):
         return self.env.seed(seed)
-    
+
     def reset(self, seed=None):
         if seed is not None:
             try:
@@ -343,7 +344,7 @@ class GymEnvWrapper():
                 self.env.real_step = bool_val
             except:
                 raise NotImplementedError
-    
+
     def evaluate_success(self, trajs):
         try:
             succ_metric = self.env.evaluate_success(trajs)
@@ -360,7 +361,7 @@ class GymEnvWrapper():
         except:
             print('No close')
             pass
-    
+
     ########################
     # Domain Randomization #
     ########################
@@ -369,7 +370,7 @@ class GymEnvWrapper():
             Randomizes dynamics parameters based on provided
             values
         """
-        for param_id in param_dict.keys(): 
+        for param_id in param_dict.keys():
             #param_id - type of dynamics parameter eg. body_mass, frictionloss etc.
             for name, dist_params in param_dict[param_id].items():
                 #name = name of body or joint etc. in mujoco xml
@@ -389,7 +390,7 @@ class GymEnvWrapper():
                     field = self.env.env.sim.model.dof_frictionloss
                 elif param_id == "geom_size":
                     idx = self.env.env.sim.model.geom_name2id(name)
-                    field = self.env.env.sim.model.geom_size           
+                    field = self.env.env.sim.model.geom_size
                 elif param_id == "geom_friction":
                     idx = self.env.env.sim.model.geom_name2id(name)
                     field = self.env.env.sim.model.geom_friction
@@ -398,7 +399,7 @@ class GymEnvWrapper():
                     field = self.env.env.sim.model.sensor_noise
                 else:
                     raise ValueError("Unknown dynamics field")
-                
+
                 if name not in self.default_dyn_params[param_id].keys():
                     curr_val = deepcopy(field[idx])
                     self.default_dyn_params[param_id][name] = curr_val #update default params only first time
@@ -406,7 +407,7 @@ class GymEnvWrapper():
                     curr_val = deepcopy(self.default_dyn_params[param_id][name])
 
                 biased_mean = (1.0 + bias_scale) * curr_val
-                rand_val = self.env.np_random.uniform(biased_mean - biased_mean * noise_scale, 
+                rand_val = self.env.np_random.uniform(biased_mean - biased_mean * noise_scale,
                                                       biased_mean + biased_mean * noise_scale)
                 # rand_val = biased_mean + noise_sample
                 #update the relevant field and idx
@@ -424,20 +425,20 @@ class GymEnvWrapper():
                 self.env.render()
             except:
                 print('Rendering not available')
-                pass 
+                pass
 
-    def get_curr_frame(self, frame_size=(640,480), 
+    def get_curr_frame(self, frame_size=(640,480),
                        camera_name=None, device_id=0):
         try:
-            curr_frame = self.env.env.sim.render(width=frame_size[0], height=frame_size[1], 
+            curr_frame = self.env.env.sim.render(width=frame_size[0], height=frame_size[1],
                                                 mode="offscreen", camera_name=camera_name, device_id=device_id)
             return curr_frame[::-1,:,:]
         except:
             try:
-                curr_frame = self.env.env.sim.render(width=frame_size[0], height=frame_size[1], 
+                curr_frame = self.env.env.sim.render(width=frame_size[0], height=frame_size[1],
                                                     mode="offscreen", camera_name=camera_name, device_id=device_id)
             except:
-                raise NotImplementedError('Getting frame not implemented')    
+                raise NotImplementedError('Getting frame not implemented')
 
 
 
@@ -458,18 +459,18 @@ if __name__ == "__main__":
 
     def rollout_fn(u_vec: np.ndarray):
         """
-        Given a batch of sequences of actions, rollout 
+        Given a batch of sequences of actions, rollout
         in sim envs and return sequence of costs
         """
         obs_vec, rew_vec, done_vec, _ = rollout_env.rollout(u_vec.copy())
         return -1.0*rew_vec #we assume environment returns rewards, but controller needs consts
-    
+
     #Create functions for controller
     def set_sim_state_fn(state_dict: dict):
         """
         Set state of simulation environments for rollouts
         """
-        rollout_env.set_env_state(state_dict)   
+        rollout_env.set_env_state(state_dict)
 
     with open("../../examples/configs/hand/pen-v0.yml") as file:
         exp_params = yaml.load(file, Loader=yaml.FullLoader)
